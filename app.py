a9990e162c67e40ab7749852e6ba6a1d383ede1a
Atish-Kr-Sharma73 <atish.sharma7321@gmail.com>
2025-01-21 22:36:26 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('CampusX')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('1687467329658.jpg')
+
+with col2:
+    st.write("""
+    At its core, CampusX aims to change this education system of India. We believe that high-quality education is not just for the privileged few. It is the right of everyone who seeks it. Through our mentorship program, we aim to bring quality education to every single student. A mentored student is provided with guidance on how to ace a technology through 24x7 mentorship, live and recorded video lectures, daily skill-building activities, project assignments, and evaluation, hackathons, interactions with industry experts, soft skill training, personal counseling, and comprehensive reports. All we need from you is intent, a ray of passion to learn.
+    """)
