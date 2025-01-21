a37c1ca86efd1644d9d31be6175b59f171979df7
Atish-Kr-Sharma73 <atish.sharma7321@gmail.com>
2025-01-21 22:46:40 +0530

    add courses to home page

--- app.py
+++ app.py
@@ -11,3 +11,8 @@ with col2:
     st.write("""
     At its core, CampusX aims to change this education system of India. We believe that high-quality education is not just for the privileged few. It is the right of everyone who seeks it. Through our mentorship program, we aim to bring quality education to every single student. A mentored student is provided with guidance on how to ace a technology through 24x7 mentorship, live and recorded video lectures, daily skill-building activities, project assignments, and evaluation, hackathons, interactions with industry experts, soft skill training, personal counseling, and comprehensive reports. All we need from you is intent, a ray of passion to learn.
     """)
+
+st.header('Course Offered')
+st.subheader('Data science')
+st.subheader('Data Analysis')
+st.subheader('Data Engineering')
